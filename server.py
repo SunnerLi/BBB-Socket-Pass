d0391ba0ea1ad7fa9cc78d4bb349350681eb4bea
SunnerLi <a6214123@gmail.com>
2015-08-25 10:05:17 +0800

    add time to pass

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import sys
 
 tcpPort = 13000	#port number
 imageLength = 512	#image length each time we read
-passTime = 100	#time to pass the image
+passTime = 1000	#time to pass the image
 
 if __name__ == "__main__":
 	#connection set
