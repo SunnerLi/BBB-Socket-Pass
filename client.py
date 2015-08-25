4e954b4739b9563e9bf671b360a5ee9c39629fa9
SunnerLi <a6214123@gmail.com>
2015-08-25 10:18:09 +0800

    change the passing time

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 hostName = '192.168.7.2'
 tcpPort = 13000	#port number
 imageLength = 512	#image length each time we read
-passTime = 10	#time to pass the image
+passTime = 2	#time to pass the image
 
 if __name__ == "__main__":
 	#connection set
